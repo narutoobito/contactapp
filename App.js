b5a3178a36870e6483767db93bd40566153f5f9e
Udit Sharma <lunaticfrienge.udit@rediffmail.com>
2021-05-20 20:49:15 +0530

    working on authentication online mode

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 //import trial from './trial'
 //export default trial;
+//import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+//import SettingScreen from './screens/settingscreen';
+//import contacts ,{compareNames} from './contacts';
+//import Row from './Row';
+
+
+
+
 
 import React from 'react';
 import { StyleSheet, Text, View ,Button} from 'react-native';
-import contacts ,{compareNames} from './contacts';
-import Row from './Row';
 import {createSwitchNavigator, createAppContainer} from 'react-navigation';
-//import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import AddContactScreen from './screens/addContactScreen';
 import ContactListScreen from './screens/contactListScreen';
 import {createStackNavigator} from 'react-navigation-stack';
 import contactDetailScreen from './screens/contactdetailsscreen';
 import loginScreen from './screens/loginScreen';
-import SettingScreen from './screens/settingscreen';
 import {fetchUser} from './getContacts'
+
+
 
 const MainNavigator=createStackNavigator({
 AddContact: AddContactScreen,
@@ -24,6 +30,9 @@ ContactDetail: contactDetailScreen,
 initialRouteName: 'ContactList'
 }
 )
+
+
+
 //const ContactContainer=createAppContainer(ContactTab);
 //
 //const MainNavigator=createBottomTabNavigator({
@@ -63,8 +72,6 @@ fetchUser().then(results=> this.setState({contacts: results}));
  return(
 
  <AppContainer screenProps={{contacts: this.state.contacts, AddContacts:this.addContacts}}/>
-
-
  );
  }
 }
--- screens/loginScreen.js
+++ screens/loginScreen.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import {View,Text,Button,TextInput} from 'react-native'
+import {View, Text ,Button, TextInput,StyleSheet} from 'react-native'
 
 export default class LoginScreen extends React.Component
 {
 state={
 username: '',
-password: ''
+password: '',
+warnings: 'type username and password',
 }
-
+//http://192.168.43.208:2018/
 _handleNavigatiion= async ()=>{
-resp=
-fetch('http://localhost:2018/',
-{
-method:'POST' ,
-headers:
-{'Content-Type' : 'application/json'},
-body: JSON.stringify({username: this.state.username, password: this.state.password })
-})
-.then(res=>{resp=res; console.log(res)});
-if(resp.ok)
-this.props.navigation.navigate('Main')
+        let result=null;
+        
+        let response = await fetch('https://narutoobito-autherization-1.glitch.me/',{
+                                    method:'POST' ,
+                                    headers:
+                                            {'Content-Type' : 'application/json'},
+                                            body: JSON.stringify({username: this.state.username, password: this.state.password })
+                                            })//.then(res=>res.json()).then(r=>{if(r.ans==" correct username")this.props.navigation.navigate('Main'); console.log(r)});
+        result = await response.json()
+        if(result){
+        if(result.ans==" correct username")
+            this.props.navigation.navigate('Main')
+        else this.setState({warnings:result.ans})
+        }
 }
 
 //const options={
@@ -45,15 +49,34 @@ return(
 placeholder="username"
 value={this.state.username}
 onChangeText={this.handleNameChange}
+style={styles.inputs}
 />
 <TextInput
 placeholder="password"
 value={this.state.password}
 onChangeText={this.handlePasswordChange}
+style={styles.inputs}
 />
 <Button title="Press Me" onPress={this._handleNavigatiion}></Button>
+<Text style={styles.warnings}>{this.state.warnings}</Text>
 </View>
 )
 
 }
 }
+
+
+styles=StyleSheet.create({
+ inputs:{
+    padding: 10,
+    backgroundColor:"orange",
+    margin:10,
+    width: "90%",
+ },
+
+ warnings:{
+     margin:50,
+    color: "red",
+
+ }
+})
